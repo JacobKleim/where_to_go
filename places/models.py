ffb87da231c0562ef0e6966b876ef6c7d2c9df93
JacobKleim <yakovkleimenov@yandex.ru>
2024-04-11 18:16:27 +0400

    Add model for images

--- places/models.py
+++ places/models.py
@@ -10,3 +10,14 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    image = models.ImageField()
+    place = models.ForeignKey(
+        Place, related_name='images',
+        on_delete=models.CASCADE)
+    number = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.number} {self.place.title}'
