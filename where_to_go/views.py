a0f5cfab8472c28c12435196b0fa18997b1200e5
JacobKleim <yakovkleimenov@yandex.ru>
2024-04-14 14:41:08 +0400

    Replaced the test data with data from the database

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,48 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 from django.http.response import JsonResponse
 
-import json
-
 from places.models import Place
-
-
-def show_start_page(request):
-    places = Place.objects.all()
-    places_geojson = {
-        "type": "FeatureCollection",
-        "features": []
-    }
-    for place in places:
-        images_urls = [image.image.url for image in place.images.all()]
-        print(images_urls)
-        images_urls_json = json.dumps(images_urls)
-        print(images_urls)
-        place_data = {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [place.lng, place.lat]
-            },
-            "properties": {
-                "title": place.title,
-                "placeId": place.id,
-                "detailsUrl": 
-                "static/places/moscow_legends.json",
-                    # {
-                    #     "title": place.title,
-                    #     "imgs": images_urls_json,
-                    #     "description_short": place.description_short,
-                    #     "description_long": place.description_long,
-                    #     "coordinates": {
-                    #         "lng": place.lng,
-                    #         "lat": place.lat,
-                    #     }
-                    # },
-            }
-        }
-        places_geojson["features"].append(place_data)
-
-    return render(request, 'index.html', {'places_geojson': places_geojson})
 
 
 def get_place(request, id):
@@ -59,3 +19,27 @@ def get_place(request, id):
     return JsonResponse(serialized_place,
                         json_dumps_params={'indent': 2, 'ensure_ascii': False},
                         safe=False)
+
+
+def show_start_page(request):
+    places = Place.objects.all()
+    places_geojson = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+    for place in places:
+        place_data = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.lng, place.lat]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": reverse(get_place, kwargs={'id': place.id}),
+            }
+        }
+        places_geojson["features"].append(place_data)
+
+    return render(request, 'index.html', {'places_geojson': places_geojson})
