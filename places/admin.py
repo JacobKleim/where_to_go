4f5f1037826793e7800ee4e0d7f03b5fc4ecabbd
JacobKleim <yakovkleimenov@yandex.ru>
2024-04-16 15:04:04 +0400

    Added image preview in ImageInline in the admin panel

--- places/admin.py
+++ places/admin.py
@@ -1,17 +1,32 @@
 from django.contrib import admin
-
+from django.utils.html import format_html
 from .models import Place, Image
 
 
-admin.site.register(Image)
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
 
 
 class ImageInline(admin.TabularInline):
     model = Image
 
+    readonly_fields = ['get_preview']
+    fields = ['place', 'image', 'get_preview', 'number']
+
+    def get_preview(self, obj):
+        try:
+            return format_html(
+                '<img src="{}" style="max-width: 200px; max-height: 200px;" />'
+                .format(obj.image.url))
+        except Exception as e:
+            return format_html(
+                '<span style="color:red;">Error: {}</span>'.format(str(e)))
+
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
+
     inlines = [
         ImageInline,
     ]
