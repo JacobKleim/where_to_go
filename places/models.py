a689dc572845edb7b5056c3dfd4d81530e34618f
JacobKleim <yakovkleimenov@yandex.ru>
2024-04-16 15:58:45 +0400

    Added text editor to admin panel

--- places/models.py
+++ places/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+from tinymce.models import HTMLField
+
 
 class Place(models.Model):
     title = models.TextField()
     description_short = models.TextField()
-    description_long = models.TextField()
+    description_long = HTMLField()
     lng = models.DecimalField(max_digits=18, decimal_places=15)
     lat = models.DecimalField(max_digits=18, decimal_places=15)
 
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,5 +9,6 @@ from where_to_go import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.show_start_page),
-    path('place/<int:id>/', views.get_place)
+    path('place/<int:id>/', views.get_place),
+    path('tinymce/', include('tinymce.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
